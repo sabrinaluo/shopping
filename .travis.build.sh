b97a20cfc4b04091ebfe8f3bdff671d419b38e86
sabrinaluo <luohaojing@gmail.com>
2016-06-02 21:46:28 +0800

    update travis

---
+++ .travis.build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ..
+git clone --depth=50 --branch=master https://github.com/sabrinaluo/shopping-client.git shopping-client
+cd shopping-client && npm install && npm run build
+cp -r dist/* ../shopping/public
